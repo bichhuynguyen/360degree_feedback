52410be3380670852dcac5debdc1602d9786280d
Huy Bich Nguyen <huynb@asiantech.vn>
2015-07-21 17:46:37 +0700

    edited style

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150721091538) do
+ActiveRecord::Schema.define(version: 20150721095524) do
 
   create_table "roles", force: :cascade do |t|
     t.string   "name",       limit: 255
